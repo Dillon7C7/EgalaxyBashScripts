92e575382c7fbaa752e862b402f934e647455022
Dillon Dommer <dillon.dommer@gmail.com>
2019-08-01 17:11:40 -0400

    Fix ffprobe frame rate checking

--- sk_encode.sh
+++ sk_encode.sh
@@ -5,12 +5,9 @@ FFMPEG=/usr/bin/ffmpeg
 HB=/usr/local/bin/HandBrakeCLI
 WM_DIR=./watermarks
 
+ffprobe=/usr/bin/ffprobe
+opts=( -v error -select_streams v -show_entries stream=r_frame_rate -of csv=p=0 )
  
- 
-#if [ "$1" -eq "" ]; then
-#  echo "Usage: $0 [inputfile]"
-#  exit 1
-#fi
 
 if [ "$#" -ne 1 ] || ! [ -f "$1" ]; then
   echo "Usage: $0 file" >&2
@@ -19,6 +16,20 @@ fi
 
 INPUT="$1"
  
+# ${ffprobe_frame_rate} should be of the form num/num.
+# Ex. 30000/1001 or 2997/100
+ffprobe_frame_rate="$(${ffprobe} "${opts[@]}" "${INPUT}")"
+
+# Divide the numbers in ${ffprobe_frame_rate}
+real_frame_rate=$((ffprobe_frame_rate))
+
+# make sure the frame rate (rounded down) is 29
+if [ "${real_frame_rate}" -ne 29 ]; then
+	echo "ERROR!!!! Segment does not have a floor frame rate of 29 !!" >&2
+	echo "Frame rate is ${real_frame_rate}" >&2
+	exit 1
+fi
+
 $HB 	-i   ${INPUT} -e x264 -q 13 -E faac --ab 320 --decomb  --x264-preset faster --x264-tune film  --x264-profile high -x level=4.1 --crop 0:0:0:0 -o ${INPUT/.mov/}.mp4  --strict-anamorphic
 
 chmod 777  ${INPUT/.mov/}.mp4
@@ -33,7 +44,7 @@ echo "Copying to REMOTE_HOST!"
 #scp  /home/encoder/w/done_archive/sk/${INPUT/.mov/}.mp4     USER@REMOTE_HOST:REMOTE_DIRECTORY
 #scp  /home/encoder/w/done_archive/sk/${INPUT/.mov/}.mp4     USER@REMOTE_HOST:/REMOTE_DIRECTORY
 scp  /home/encoder/w/done_archive/sk/out/${INPUT/.mov/}.mp4     USER@REMOTE_HOST.com:/REMOTE_DIRECTORY
-#scp -P 2222 /home/encoder/w/done_archive/sk/out/${INPUT/.mov/}.mp4     nnencoder@192.168.7.106:/var/www/html/shows/
+#scp -P PORT /home/encoder/w/done_archive/sk/out/${INPUT/.mov/}.mp4     nnencoder@192.168.7.106:/var/www/html/shows/
 echo "---------------------------------------"
 
 if [ "$?" -eq "0" ]; then
