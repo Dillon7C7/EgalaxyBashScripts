e59371584b184e1a26a928ad34bc8a376b5a26ca
Dillon Dommer <dillon.dommer@gmail.com>
2020-12-21 16:30:04 -0500

    Change frame rate check from rounded down 29 to 29.97

--- sk_encode.sh
+++ sk_encode.sh
@@ -9,7 +9,7 @@ frame_opts=( -v error -select_streams v -show_entries stream=r_frame_rate -of cs
 codec_opts=( -v error -select_streams v -show_entries stream=codec_name \
 	-of default=noprint_wrappers=1:nokey=1 )
 
-if [ "$#" -ne 1 ] || ! [ -f "$1" ]; then
+if [ $# -ne 1 ] || ! [ -f "$1" ]; then
 	echo "Usage: $0 file" >&2
 	exit 1
 fi
@@ -18,13 +18,13 @@ INPUT="$1"
  
 # ${ffprobe_frame_rate} should be of the form num/num.
 # Ex. 30000/1001 or 2997/100
-ffprobe_frame_rate="$(${ffprobe} "${opts[@]}" "${INPUT}")"
+ffprobe_frame_rate="$(${ffprobe} "${frame_opts[@]}" "${INPUT}")"
 
-# Divide the numbers in ${ffprobe_frame_rate}
-real_frame_rate=$((ffprobe_frame_rate))
+# Get the frame rate to 2 decimal places
+real_frame_rate=$(echo "scale=2; ${ffprobe_frame_rate}" | bc)
 
-# make sure the frame rate (rounded down) is 29
-if [ "${real_frame_rate}" -ne 29 ]; then
+# make sure the frame rate is 29.97
+if [[ "${real_frame_rate}" != "29.97" ]]; then
 	echo "ERROR!!!! Segment does not have a floor frame rate of 29 !!" >&2
 	echo "Frame rate is ${real_frame_rate}" >&2
 	exit 1
@@ -51,9 +51,9 @@ echo "---------------------------------------"
 echo "Copying to REMOTE_HOST!"
 
 #scp  /home/encoder/w/done_archive/sk/${INPUT/.mov/}.mp4     USER@REMOTE_HOST:REMOTE_DIRECTORY
-#scp  /home/encoder/w/done_archive/sk/${INPUT/.mov/}.mp4     USER@REMOTE_HOST:/REMOTE_DIRECTORY
-scp  /home/encoder/w/done_archive/sk/out/${INPUT/.mov/}.mp4     USER@REMOTE_HOST.com:/REMOTE_DIRECTORY
-#scp -P PORT /home/encoder/w/done_archive/sk/out/${INPUT/.mov/}.mp4     nnencoder@192.168.7.106:/var/www/html/shows/
+#scp  /home/encoder/w/done_archive/sk/${INPUT/.mov/}.mp4     USER@REMOTE_HOST:REMOTE_DIRECTORY
+scp  /home/encoder/w/done_archive/sk/out/${INPUT/.mov/}.mp4     USER@REMOTE_HOST:REMOTE_DIRECTORY
+#scp -P 2222 /home/encoder/w/done_archive/sk/out/${INPUT/.mov/}.mp4     nnencoder@192.168.7.106:/var/www/html/shows/
 echo "---------------------------------------"
 
 if [ "$?" -eq "0" ]; then
