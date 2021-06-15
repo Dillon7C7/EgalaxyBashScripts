d8fd174cc47cddaf358dc8fae8117047f6259672
Dillon Dommer <dillon.dommer@gmail.com>
2021-06-15 01:46:36 -0400

    Refactor main encoding script

--- sk_encode.sh
+++ sk_encode.sh
@@ -1,64 +1,103 @@
 #!/bin/bash
 
-FFMPEG=/usr/bin/ffmpeg
-HB=/usr/local/bin/HandBrakeCLI
-WM_DIR=./watermarks
+script="${0##*/}"
+done_archive="${HOME}/w/done_archive/sk/out"
 
-ffprobe=/usr/bin/ffprobe
-frame_opts=( -v error -select_streams v -show_entries stream=r_frame_rate -of csv=p=0 )
-codec_opts=( -v error -select_streams v -show_entries stream=codec_name \
-	-of default=noprint_wrappers=1:nokey=1 )
+ffprobe_v_opts=( -v error -select_streams v -show_entries "stream=codec_name,width,height,r_frame_rate" -of "csv=p=0" )
+ffprobe_a_opts=( -v error -select_streams a -show_entries "stream=index" -of "csv=p=0" )
 
-if [ $# -ne 1 ] || ! [ -f "$1" ]; then
-	echo "Usage: $0 file" >&2
+hb_opts=( -e x264 -E faac --ab 320 --decomb -q 13 --x264-preset faster
+	--x264-tune film --x264-profile high -x "level=4.1"
+	--crop 0:0:0:0 --strict-anamorphic -o "$output")
+
+print_e_and_exit()
+{
+	err_msg="$1"
+	printf '%s\n' "${script}: ERROR: ${err_msg}"
 	exit 1
-fi
+} >&2
 
-INPUT="$1"
- 
-# ${ffprobe_frame_rate} should be of the form num/num.
-# Ex. 30000/1001 or 2997/100
-ffprobe_frame_rate="$(${ffprobe} "${frame_opts[@]}" "${INPUT}")"
+# check args given to this script
+check_args()
+{
+	if [[ $# -ne 1 ]] || [[ ! -f "$1" ]]; then
+		print_e_and_exit "requires one argument: a video file"
+	else
+		input="$1"
+		output="${input/%mov/mp4}"
+	fi
 
-# Get the frame rate to 2 decimal places
-real_frame_rate=$(echo "scale=2; ${ffprobe_frame_rate}" | bc)
+	return 0
+}
 
-# make sure the frame rate is 29.97
-if [[ "${real_frame_rate}" != "29.97" ]]; then
-	echo "ERROR!!!! Segment does not have a floor frame rate of 29 !!" >&2
-	echo "Frame rate is ${real_frame_rate}" >&2
-	exit 1
-fi
+# check video stream entries
+check_v_stream()
+{
+	# we must get codec, resolution, frame rate
+	IFS=, read -r codec width height r_frame_rate <<< "$(ffprobe -i "$input" "${ffprobe_v_opts[@]}")"
 
-# Check video codec of segment; should be Apple ProRes 422 ("prores")
-video_codec="$(${ffprobe} "${codec_opts[@]}" "${INPUT}")"
+	#  if we need to divide in the future for other numer/denom pairs...
+	#~ real_frame_rate=$(echo "scale=2; ${r_frame_rate}" | bc)
 
-if [[ "$video_codec" != "prores" ]]; then
-	echo "ERROR!!!! Segment was not encoded with Apple ProRes 422 !!" >&2
-	echo "Video codec is ${video_codec}" >&2
-	exit 1
-fi
+	if [[ "$codec" != "prores" ]]; then
+		print_e_and_exit "$input: video stream must be Apple ProRes. Given: $codec"
+	elif [[ $width -ne 1920 ]] || [[ $height -ne 1080 ]]; then
+		print_e_and_exit "$input: video resolution must be 1920x1080. Given: ${width}x${height}"
+	elif [[ "$r_frame_rate" != "30000/1001" ]]; then
+		print_e_and_exit "$input: real frame rate must be 30000/1001. Given: $r_frame_rate"
+	fi
 
-$HB 	-i   ${INPUT} -e x264 -q 13 -E faac --ab 320 --decomb  --x264-preset faster --x264-tune film  --x264-profile high -x level=4.1 --crop 0:0:0:0 -o ${INPUT/.mov/}.mp4  --strict-anamorphic
+	return 0
+}
 
-chmod 777  ${INPUT/.mov/}.mp4
-echo "PRE ENCODING DONE !!!!!!"
-echo "---------------------------------------"
-echo "Copying to local"
+# check audio stream entries
+check_a_stream()
+{
+	num_a_streams=$(ffprobe -i "$input" "${ffprobe_a_opts[@]}" | wc -w)
 
-mv ${INPUT/.mov/}.mp4  /home/encoder/w/done_archive/sk/out/
-echo "---------------------------------------"
-echo "Copying to REMOTE_HOST!"
+	# make sure we only have 1 audio stream. will allow for a variable number in the future
+	if [[ $num_a_streams -ne 1 ]]; then
+		print_e_and_exit "$input: must only have 1 audio stream. Given: $num_a_streams"
+	fi
 
-#scp  /home/encoder/w/done_archive/sk/${INPUT/.mov/}.mp4     USER@REMOTE_HOST:REMOTE_DIRECTORY
-#scp  /home/encoder/w/done_archive/sk/${INPUT/.mov/}.mp4     USER@REMOTE_HOST:REMOTE_DIRECTORY
-scp  /home/encoder/w/done_archive/sk/out/${INPUT/.mov/}.mp4     USER@REMOTE_HOST:REMOTE_DIRECTORY
-#scp -P 2222 /home/encoder/w/done_archive/sk/out/${INPUT/.mov/}.mp4     nnencoder@192.168.7.106:/var/www/html/shows/
-echo "---------------------------------------"
+	return 0
+}
 
-if [ "$?" -eq "0" ]; then
-	echo "${INPUT/.mov/}.mp4 done 100%"
-else
-	echo "$0 ERROR: scp of ${INPUT/.mov/}.mp4 failed." >&2
-	exit 1
-fi
+encode_and_upload()
+{
+	# if there is an abnormal exit during encoding, remove output file
+	trap 'rm -fv "$output"' HUP INT QUIT TERM
+
+	if ! /usr/local/bin/HandBrakeCLI -i "$input" "${hb_opts[@]}" -o "$output"; then
+		print_e_and_exit "$input: encoding failed"
+	fi
+
+	trap - HUP INT QUIT TERM
+
+	chmod 777 "$output"
+	printf '%s\n' "Pre-encoding complete !!!!!!"
+	printf '%s\n' "---------------------------------------"
+	printf '%s\n' "Moving to local archive..."
+
+	# mv will remove the destination in case of error, no need for a trap
+	if ! mv "$output" "${done_archive}/"; then
+		print_e_and_exit "$output: mv to ${done_archive}/ failed"
+	fi
+
+	printf '%s\n' "---------------------------------------"
+	printf '%s\n' "Uploading to beer.nakednews..."
+
+	if ! scp "${done_archive}/${output}" REMOTE_USER@REMOTE_HOST:REMOTE_URI; then
+		print_e_and_exit "$input: upload failed"
+	fi
+
+	printf '%s\n' "---------------------------------------"
+	printf '%s\n' "${output} done 100%"
+
+	return 0
+}
+
+check_args "$@" && \
+check_v_stream  && \
+check_a_stream  && \
+encode_and_upload
