f20d1179c053c0bcbb58d4a70925679107b6c215
Dillon Dommer <dillon.dommer@gmail.com>
2021-06-15 01:51:39 -0400

    Fix resolution check bug
    The video stream resolution check previously looked for a width of 1920
    OR a height of 1080. This has been changed to AND.
    
    The function that prints errors has also been renamed to 'die()'

--- sk_encode.sh
+++ sk_encode.sh
@@ -10,10 +10,12 @@ hb_opts=( -e x264 -E faac --ab 320 --decomb -q 13 --x264-preset faster
 	--x264-tune film --x264-profile high -x "level=4.1"
 	--crop 0:0:0:0 --strict-anamorphic -o "$output")
 
-print_e_and_exit()
+
+die()
 {
 	err_msg="$1"
 	printf '%s\n' "${script}: ERROR: ${err_msg}"
+
 	exit 1
 } >&2
 
@@ -21,7 +23,7 @@ print_e_and_exit()
 check_args()
 {
 	if [[ $# -ne 1 ]] || [[ ! -f "$1" ]]; then
-		print_e_and_exit "requires one argument: a video file"
+		die "requires one argument: a video file"
 	else
 		input="$1"
 		output="${input/%mov/mp4}"
@@ -40,11 +42,11 @@ check_v_stream()
 	#~ real_frame_rate=$(echo "scale=2; ${r_frame_rate}" | bc)
 
 	if [[ "$codec" != "prores" ]]; then
-		print_e_and_exit "$input: video stream must be Apple ProRes. Given: $codec"
-	elif [[ $width -ne 1920 ]] || [[ $height -ne 1080 ]]; then
-		print_e_and_exit "$input: video resolution must be 1920x1080. Given: ${width}x${height}"
+		die "$input: video stream must be Apple ProRes. Given: $codec"
+	elif [[ $width -ne 1920 ]] && [[ $height -ne 1080 ]]; then
+		die "$input: video resolution must be 1920x1080. Given: ${width}x${height}"
 	elif [[ "$r_frame_rate" != "30000/1001" ]]; then
-		print_e_and_exit "$input: real frame rate must be 30000/1001. Given: $r_frame_rate"
+		die "$input: real frame rate must be 30000/1001. Given: $r_frame_rate"
 	fi
 
 	return 0
@@ -57,7 +59,7 @@ check_a_stream()
 
 	# make sure we only have 1 audio stream. will allow for a variable number in the future
 	if [[ $num_a_streams -ne 1 ]]; then
-		print_e_and_exit "$input: must only have 1 audio stream. Given: $num_a_streams"
+		die "$input: must only have 1 audio stream. Given: $num_a_streams"
 	fi
 
 	return 0
@@ -69,7 +71,7 @@ encode_and_upload()
 	trap 'rm -fv "$output"' HUP INT QUIT TERM
 
 	if ! /usr/local/bin/HandBrakeCLI -i "$input" "${hb_opts[@]}" -o "$output"; then
-		print_e_and_exit "$input: encoding failed"
+		die "$input: encoding failed"
 	fi
 
 	trap - HUP INT QUIT TERM
@@ -81,14 +83,14 @@ encode_and_upload()
 
 	# mv will remove the destination in case of error, no need for a trap
 	if ! mv "$output" "${done_archive}/"; then
-		print_e_and_exit "$output: mv to ${done_archive}/ failed"
+		die "$output: mv to ${done_archive}/ failed"
 	fi
 
 	printf '%s\n' "---------------------------------------"
 	printf '%s\n' "Uploading to beer.nakednews..."
 
 	if ! scp "${done_archive}/${output}" REMOTE_USER@REMOTE_HOST:REMOTE_URI; then
-		print_e_and_exit "$input: upload failed"
+		die "$input: upload failed"
 	fi
 
 	printf '%s\n' "---------------------------------------"
